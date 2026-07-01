623fee8efcbf79bb046c9f73abae6a49813a3f4c
Dimitri <dimitri.s.amado@gmail.com>
2026-07-01 12:20:56 -0300

    feat: Implement Order registry

---
+++ src/use_cases/registry_order.py
@@ -0,0 +1,39 @@
+from src.models.repository.interfaces.orders import OrdersRepositoryInterface
+from src.main.http_types.http_request import HttpResponse
+from datetime import datetime
+
+class RegistryOrder:
+    def __init__(self, order_repository: OrdersRepositoryInterface) -> None:
+        self.order_repository = order_repository
+
+    def registry(self, http_request: HttpRequest) -> HttpResponse:
+        try:
+            body = http_request.body
+            new_order = self.__format_new_order(body)
+            self.__registry_order(new_order)
+            return self.__format_response()
+        except Exception as exception:
+            return HttpResponse(
+                body={ "error": str(exception) },
+                status_code=400
+            )
+
+    def __format_new_order(self, body: dict) -> dict:
+        new_order = body["data"]
+        new_order = { **new_order, "created_at": datetime.now()}
+        return new_order
+
+    def __registry_order(self, new_order: dict) -> dict:
+        self.__orders_repository.insert_document(new_order)
+    
+    def __format_response(self) -> HttpResponse:
+        return HttpResponse(
+            body={
+                "data": {
+                    "type": "Order",
+                    "count": 1,
+                    "registry": True
+                }
+            },
+            status_code=201
+        )
