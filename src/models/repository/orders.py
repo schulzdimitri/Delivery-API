803a263237f6620aed3e013347d3b18e556133cb
Dimitri <dimitri.s.amado@gmail.com>
2026-07-01 08:52:38 -0300

    fix: correct import path, fix collection method invocation

--- src/models/repository/orders.py
+++ src/models/repository/orders.py
@@ -1,4 +1,4 @@
-from models.connection.connection import DBConnection
+from src.models.connection.connection import DBConnection
 
 class OrdersRepository:
     def __init__(self, db_connection: DBConnection) -> None:
@@ -6,9 +6,9 @@ class OrdersRepository:
         self.__db_connection = db_connection
         
     def insert_document(self, document: dict) -> dict:
-        collection = self.__db_connection.get_collection[self.__collection_name]
+        collection = self.__db_connection.get_collection(self.__collection_name)
         collection.insert_one(document)
 
     def insert_list_of_documents(self, list_of_documents: list[dict]) -> None:
-        collection = self.__db_connection.get_collection[self.__collection_name]
+        collection = self.__db_connection.get_collection(self.__collection_name)
         collection.insert_many(list_of_documents)
