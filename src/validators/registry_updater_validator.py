513148983637ee7e18c5623aa8d057a5afa769d4
Dimitri <dimitri.s.amado@gmail.com>
2026-07-08 10:27:42 -0300

    feat: Implement registry updater with validator

---
+++ src/validators/registry_updater_validator.py
@@ -0,0 +1,19 @@
+from cerberus import Validator
+from src.errors.types.http_unprocessable_entity import HttpUnprocessableEntityError
+
+def registry_updater_validator(body: any):
+    body_validator = Validator({
+        'data': {
+            'type': 'dict', 
+            'schema': {
+                'name': {'type': 'string'},
+                'address': {'type': 'string'},
+                'cupom': {'type': 'boolean'},
+            }
+        }
+    })
+    response = body_validator.validate(body)
+
+    if not response:
+        raise HttpUnprocessableEntityError(body_validator.errors)
+    
